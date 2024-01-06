df7293a1a4a5d57732bd11cb31d6e41e24575c83
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2024-01-05 23:12:24 -0500

    more test
    test3
    
    test4
    
    test5
    
    test5
    
    test5
    
    test5
    
    test5
    
    test5
    
    test5
    
    try again.
    
    try again.
    
    try again.
    
    try again.
    
    try again.
    
    try again.
    
    try again.
    
    try again.
    
    try again.

--- be/cmd/migrations.go
+++ be/cmd/migrations.go
@@ -13,10 +13,12 @@ import (
 
 var direction string
 var seed bool
+var env string
 
 func init() {
 	migrationsCMD.Flags().StringVarP(&direction, "direction", "d", "", "Direction of migrations (up or down)")
 	migrationsCMD.Flags().BoolVarP(&seed, "seed", "s", false, "Seed the database")
+	migrationsCMD.Flags().StringVarP(&env, "env", "e", "local", "Environment to run migrations on")
 	rootCMD.AddCommand(migrationsCMD)
 }
 
@@ -27,13 +29,15 @@ var migrationsCMD = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Running migrations")
 
-		envPath := "./infra/envs/local/.env"
-		err := godotenv.Load(envPath)
-
-		if err != nil {
-			log.Fatalf("error loading .env file: %v", err)
+		if env != "local" {
+			envPath := "./infra/envs/local/.env"
+			err := godotenv.Load(envPath)
+			if err != nil {
+				log.Fatalf("error loading .env file: %v", err)
+			}
 		}
 
+		fmt.Println(os.Getenv("DB_URL"))
 		db, err := sql.Open("mysql", os.Getenv("DB_URL"))
 		if err != nil {
 			panic(err)
